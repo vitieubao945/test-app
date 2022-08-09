7e67bf23fd1fd882fd3c7f6b47d810b4b9f06813
Bao Vo <bao@manifera.com>
2022-08-09 11:53:59 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+import './App.css';
+
+function App() {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.error('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      className="login-form"
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Email!',
+          },
+          {
+            validator: (_, value) => {
+              const regex = new RegExp("^([a-z0-9]+@[a-z0-9]+\.[a-z]{2,3})*$");
+              if (regex.test(value)) {
+                return Promise.resolve();
+              } else {
+                return Promise.reject('Email is invalid format: test@abc.com');
+              }
+            }
+          }
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default App;
